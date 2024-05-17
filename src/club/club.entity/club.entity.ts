51f1f04ed7fa0bb29a104821842af9c6143fa873
Juan David Salguero Medina <j.salguero@uniandes.edu.co>
2024-05-17 08:14:18 -0500

    logica hecha

--- src/club/club.entity/club.entity.ts
+++ src/club/club.entity/club.entity.ts
@@ -1,1 +1,25 @@
-export class ClubEntity {}
+import { SocioEntity } from "src/socio/socio.entity/socio.entity";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class ClubEntity {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    foundationDate: Date;
+
+    @Column()
+    image: string;
+
+    @Column()
+    description: string;
+
+    @ManyToMany(() => SocioEntity, socio => socio.clubs)
+    @JoinTable()
+    socios: SocioEntity[];
+}
--- src/socio/socio.entity/socio.entity.ts
+++ src/socio/socio.entity/socio.entity.ts
@@ -1,1 +1,20 @@
-export class SocioEntity {}
+import { ClubEntity } from "src/club/club.entity/club.entity";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+@Entity()
+export class SocioEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    birthday: Date;
+
+    @ManyToMany(()=> ClubEntity, club => club.socios)
+    clubs: ClubEntity[];
+}
